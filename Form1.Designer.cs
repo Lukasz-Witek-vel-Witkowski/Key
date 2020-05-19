36f51037dfe66f82827c70de7a85169479caa6e8
Lukasz Witek vel Witkowski <uwlwitkowski@gmail.com>
2020-05-19 18:18:41 +0200

    OK

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace key
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.number = new System.Windows.Forms.TextBox();
+            this.smallChars = new System.Windows.Forms.TextBox();
+            this.bigChars = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.key = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // number
+            // 
+            this.number.Location = new System.Drawing.Point(161, 37);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(100, 22);
+            this.number.TabIndex = 0;
+            // 
+            // smallChars
+            // 
+            this.smallChars.Location = new System.Drawing.Point(161, 65);
+            this.smallChars.Name = "smallChars";
+            this.smallChars.Size = new System.Drawing.Size(100, 22);
+            this.smallChars.TabIndex = 1;
+            // 
+            // bigChars
+            // 
+            this.bigChars.Location = new System.Drawing.Point(161, 93);
+            this.bigChars.Name = "bigChars";
+            this.bigChars.Size = new System.Drawing.Size(100, 22);
+            this.bigChars.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Liczba cyfr";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Liczba małych liter: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "LIczba dużych liter:";
+            // 
+            // key
+            // 
+            this.key.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.key.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.key.Location = new System.Drawing.Point(58, 131);
+            this.key.Name = "key";
+            this.key.ReadOnly = true;
+            this.key.Size = new System.Drawing.Size(379, 36);
+            this.key.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "KEY:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Generuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 284);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bigChars);
+            this.Controls.Add(this.smallChars);
+            this.Controls.Add(this.number);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox number;
+        private System.Windows.Forms.TextBox smallChars;
+        private System.Windows.Forms.TextBox bigChars;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace key
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size_Number = 0;
+            int size_SmallChars = 0;
+            int size_BigChars = 0;
+            int iter_Number = 0;
+            int iter_SmallChars = 0;
+            int iter_BigChars = 0;
+            int size = 0;
+            int value = 0;
+            string data = "";
+            bool active = false;
+            bool loop = false;
+            try
+            {
+                if (number.TextLength > 0)
+                {
+                    size_Number = int.Parse(number.Text);
+                }
+                if (smallChars.TextLength > 0)
+                {
+                    size_SmallChars = int.Parse(smallChars.Text);
+                }
+                if (bigChars.TextLength > 0)
+                {
+                    size_BigChars = int.Parse(bigChars.Text);
+                }
+            }
+            catch (FormatException)
+            {
+
+            }
+            size = size_Number + size_SmallChars+ size_BigChars;
+            Random random = new Random();
+            for(int i=0; i<size; i++)
+            {
+               
+                active = false;
+                loop = false;
+                do
+               {
+                    value = random.Next(1, 3);
+                       if (loop)
+                       {
+                           if (value == 3) value = 1;
+                           value++;
+                       }
+                    switch (value)
+                    {
+                        case 1: //liczby
+                            if (iter_Number < size_Number)
+                            {
+                                iter_Number++;
+                                active = true;
+                                value = random.Next(1, 9);
+                                data += (char)(value + 47);
+                            }
+                            break;
+                        case 2: //male litery
+                            if (iter_SmallChars < size_SmallChars)
+                            {
+                                iter_SmallChars++;
+                                active = true;
+                                value = random.Next(1, 25);
+                                data += (char)(value + 96);
+                            }
+                            break;
+                        default:// duze litery
+                            if (iter_BigChars < size_BigChars)
+                            {
+                                iter_BigChars++;
+                                active = true;
+                                value = random.Next(1, 25);
+                                data += (char)(value + 64);
+                            }
+                            break;
+                    }
+                    loop = true;
+                } while (!active);
+                
+            }
+            key.Text = data;
+        }
+    }
+}
+// liczby od 48 do 57 - 9
+// male litery 97 do 122 - 25
+//duze liter 65 do 90 - 25
